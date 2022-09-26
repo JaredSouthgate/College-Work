f9a09a2fb83db3e1e78c12054a38e6030cc973e1
JaredSouthgate <114414769+JaredSouthgate@users.noreply.github.com>
2022-09-26 14:44:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
