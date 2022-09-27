71660735631eb15cba971743e02cfc83dfadd5fa
JaredSouthgate <114414769+JaredSouthgate@users.noreply.github.com>
2022-09-27 16:42:56 +0100

    RPG game

---
+++ RPG game.cs
@@ -0,0 +1,43 @@
+﻿
+
+using System;
+class Program
+{
+    static void Main()
+    {
+       
+        Console.ForegroundColor = ConsoleColor.Magenta;
+
+        Console.WriteLine("=====================================");
+        Console.WriteLine("Hi there... My name is Lux, Im the First Star");
+        Console.WriteLine("Whats your name? ");
+        Console.WriteLine("=====================================");
+        Console.ResetColor();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine("Hi there... im Lux, the First Star");
+//Console.WriteLine("Whats your name? ");
+
+//string PlayerName = input
