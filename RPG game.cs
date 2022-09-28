7c6c96361c1a74bce5a56ef8587f5f5bc656c241
JaredSouthgate <114414769+JaredSouthgate@users.noreply.github.com>
2022-09-28 11:22:35 +0100

    RPG game
    another versiom

--- RPG game.cs
+++ RPG game.cs
@@ -1,3 +1,59 @@
+<<<<<<< HEAD
+﻿
+using System;
+class Program
+{
+    static void Main()
+    {
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Red;
+
+        Console.WriteLine("=====================================================================================");
+
+        Console.WriteLine(" Hi there... My name is Lux, Im the First Star.                                      ");
+        Console.WriteLine(" Im here to guide you through your first battle against The Fallen Stars.            ");
+        Console.WriteLine(" A group of Star Guardians who have been corrupted and lost their starlight.         ");
+        Console.WriteLine(" You, are gonna help us defeat them to reignite their starlight so they can help us  ");
+        Console.WriteLine(" with the final battle against The Anti-Star... Zoe... Are you ready?                ");
+
+        Console.WriteLine("=====================================================================================");
+        Console.ResetColor();
+
+    }
+
+
+           class Player
+        {
+            public string Name;
+            public int Level;
+            public int Health;
+            public string Race;
+            public int Damage;
+            public int GP;
+            public int Defense;
+            public int XP;
+        }
+
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=======
 ﻿
 
 using System;
@@ -41,3 +97,4 @@ class Program
 //Console.WriteLine("Whats your name? ");
 
 //string PlayerName = input
+>>>>>>> 71660735631eb15cba971743e02cfc83dfadd5fa
